a0970824709cff518fad314559d83aeafd037752
Björn Fromme <mail@bjo3rn.com>
2019-03-11 21:43:11 +0100

    First release

--- Subscriber/CronJobSubscriber.php
+++ Subscriber/CronJobSubscriber.php
@@ -46,39 +46,56 @@ class CronJobSubscriber implements SubscriberInterface
     public function onProcessStatusEmailsCronJob(\Shopware_Components_Cron_CronJob $job)
     {
         $config = $this->container->get('shopware.plugin.cached_config_reader')->getByPluginName('DpnCronStatusEmail');
+        $logger = $this->container->get('pluginlogger');
 
         $selectedOrderStatusIds = $config['dpnOrderStatus'];
-        $ordersWithChangedOrderStatus = $this->getUpdatedOrders('dpn_prev_status_order', $selectedOrderStatusIds);
+        $ordersWithChangedOrderStatus = $this->getUpdatedOrders(
+            'dpn_prev_status_order',
+            'status',
+            $selectedOrderStatusIds
+        );
         $count = $this->sendMailAndUpdateStatus('dpn_prev_status_order', $ordersWithChangedOrderStatus);
 
-        $this->container->get('pluginlogger')->info(sprintf('Order status updated on %d orders.', $count));
+        $logger->info(sprintf('Order status updated on %d orders.', $count));
 
         $selectedPaymentStatusIds = $config['dpnPaymentStatus'];
-        $ordersWithChangedPaymentStatus = $this->getUpdatedOrders('dpn_prev_status_payment', $selectedPaymentStatusIds);
+        $ordersWithChangedPaymentStatus = $this->getUpdatedOrders(
+            'dpn_prev_status_payment',
+            'cleared',
+            $selectedPaymentStatusIds
+        );
         $count = $this->sendMailAndUpdateStatus('dpn_prev_status_payment', $ordersWithChangedPaymentStatus);
 
-        $this->container->get('pluginlogger')->info(sprintf('Payment status updated on %d orders.', $count));
+        $logger->info(sprintf('Payment status updated on %d orders.', $count));
 
         return true;
     }
 
     /**
-     * @param string $table
+     * @param string $fieldPreviousStatus
+     * @param string $fieldCurrentStatus
      * @param array $selectedStatusIds
      * @return array
      */
-    protected function getUpdatedOrders($table, array $selectedStatusIds)
+    protected function getUpdatedOrders($fieldPreviousStatus, $fieldCurrentStatus, array $selectedStatusIds)
     {
         /** @var Connection $connection */
         $connection = $this->container->get('dbal_connection');
 
         $qb = $connection->createQueryBuilder();
         return $qb
-            ->select('o.id', 'o.status')
+            ->select('o.id', 'o.' . $fieldCurrentStatus . ' as status ')
             ->from('s_order', 'o')
             ->innerJoin('o', 's_order_attributes', 'a', 'o.id = a.orderID')
-            ->where($qb->expr()->in('o.status', $selectedStatusIds))
-            ->andWhere($qb->expr()->neq('a.' . $table, 'o.status'))
+            ->where(
+                $qb->expr()->andX(
+                    $qb->expr()->in('o.' . $fieldCurrentStatus, $selectedStatusIds),
+                    $qb->expr()->orX(
+                        $qb->expr()->isNull('a.' . $fieldPreviousStatus),
+                        $qb->expr()->neq('a.' . $fieldPreviousStatus, 'o.' . $fieldCurrentStatus)
+                    )
+                )
+            )
             ->execute()
             ->fetchAll();
     }
@@ -97,7 +114,14 @@ class CronJobSubscriber implements SubscriberInterface
 
         foreach ($updatedOrders as $order) {
             $mail = Shopware()->Modules()->Order()->createStatusMail($order['id'], $order['status']);
-            if ($mail !== null) {
+            if ($mail === null) {
+                $message = $this->container
+                    ->get('snippets')
+                    ->getNamespace('backend/dpn_cron_status_email/translations')
+                    ->get('auto_email_missing_template');
+                $this->container->get('pluginlogger')
+                    ->error(sprintf($message, $order['status']));
+            } else {
                 Shopware()->Modules()->Order()->sendStatusMail($mail);
                 $qb = $connection->createQueryBuilder();
                 $qb
